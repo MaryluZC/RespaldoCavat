dbba83afe1c45e3c785e40bb2eb00ca010378b6a
Maria de Lourdes Sosa Cruz <43821338+MaryluZC@users.noreply.github.com>
2022-05-16 18:59:38 -0500

    Add files via upload
    En esta carpeta se contienen los parámetros para poder mantener la sesión del usuario durante el tiempo de vida del sistema.

---
+++ InfoUsuarios/accesoLogin.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoUsuarios
+{
+    public class entrarLogin
+    {
+        public  string user{ get; set; }
+        public  string contrasena { get; set; }
+        public int opc1 { get; set; }
+         
+    }
+  
+
+
+    public class solicitudCambioPass : entrarLogin
+    {
+        public int cambioPsw { get; set; }
+    }
+
+    public class resultado
+    {
+        public int mensaje { get; set; }
+        public DataSet elDataSet { get; set; }
+    }
+
+    public class dataUser : solicitudCambioPass
+    {
+        public string nombre { get; set; }
+        public string ape1 { get; set; }
+        public string ape2 { get; set; }
+        public string correo { get; set; }
+        public string cedulaP { get; set; }
+        public int idPregunta { get; set; }
+        public string respuesta { get; set; }
+        public string telCel { get; set; }
+        public int numNotaria { get; set; }
+        public int idStatus { get; set; }
+        public int intentos { get; set; }
+        public int bloqueado { get; set; }
+        public string idTipoUser { get; set; }
+        public int idTipoDoc { get; set; }
+        public string nombreDoc { get; set; }
+    }
+    public class mnsg { public static int mensaje = 0; }
+}
---
+++ InfoUsuarios/cache/UserLoginCache.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InfoUsuarios.cache
+{
+   public static class UserLoginCache
+    {
+        public static string userCache { get; set; }
+        public static string nombre { get; set; }
+        public static string ape1 { get; set; }
+        public static string ape2 { get; set; }
+        public static string tipoUser { get; set; }
+    }
+
+    
+}
